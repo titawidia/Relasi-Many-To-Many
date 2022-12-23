34f2f64d9dbc5a2f42ce4d0b0818df5f165a2f31
titawidia <gitawidia166@gmail.com>
2022-12-23 10:17:26 +0700

    Master

---
+++ src/main/java/com/itc/manytomanyrelations/ManyToManyRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.itc.manytomanyrelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyRelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyToManyRelationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itc/manytomanyrelations/ManyToManyRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.manytomanyrelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyRelationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
